f3996703622ecfe4f0b8f7a2e1be65f9ca748c11
Michal R <vad.sol@proton.me>
2025-10-28 10:57:41 +0100

    runtime: Don't mutate `self` in `begin_partitioned_rewards`
    `Bank::begin_partitioned_rewards` was taking `&mut self`, even though
    all the calculations done by it do not require mutability and the only
    LOC requiring it was a call to `set_epoch_reward_status_calculation`.
    
    Move the `set_epoch_reward_status_calculation` call out to
    `process_new_epoch`.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1657,7 +1657,7 @@ impl Bank {
 
         let mut rewards_metrics = RewardsMetrics::default();
         // After saving a snapshot of stakes, apply stake rewards and commission
-        let (_, update_rewards_with_thread_pool_time_us) = measure_us!(self
+        let (stake_rewards, update_rewards_with_thread_pool_time_us) = measure_us!(self
             .begin_partitioned_rewards(
                 reward_calc_tracer,
                 &thread_pool,
@@ -1666,6 +1666,7 @@ impl Bank {
                 parent_height,
                 &mut rewards_metrics,
             ));
+        self.save_rewards(stake_rewards);
 
         report_new_epoch_metrics(
             epoch,
