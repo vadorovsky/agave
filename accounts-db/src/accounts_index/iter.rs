406eeabf30692c512c6503f356c8a204d8f6ee8d
Brooks <brooks@anza.xyz>
2026-03-25 12:18:32 -0400

    Removes AccountsIndexPubkeyIterOrder (#11509)

--- accounts-db/src/accounts_index/iter.rs
+++ accounts-db/src/accounts_index/iter.rs
@@ -10,18 +10,16 @@ pub struct AccountsIndexPubkeyIterator<'a, T: IndexValue, U: DiskIndexValue + Fr
     account_maps: &'a [Arc<InMemAccountsIndex<T, U>>],
     current_bin: usize,
     items: Vec<Pubkey>,
-    iter_order: AccountsIndexPubkeyIterOrder,
 }
 
 impl<'a, T: IndexValue, U: DiskIndexValue + From<T> + Into<T>>
     AccountsIndexPubkeyIterator<'a, T, U>
 {
-    pub fn new(index: &'a AccountsIndex<T, U>, iter_order: AccountsIndexPubkeyIterOrder) -> Self {
+    pub fn new(index: &'a AccountsIndex<T, U>) -> Self {
         Self {
             account_maps: &index.account_maps,
             current_bin: 0,
             items: Vec::new(),
-            iter_order,
         }
     }
 }
@@ -39,27 +37,12 @@ impl<T: IndexValue, U: DiskIndexValue + From<T> + Into<T>> Iterator
 
             let map = &self.account_maps[self.current_bin];
             let mut items = map.keys();
-            if self.iter_order == AccountsIndexPubkeyIterOrder::Sorted {
-                items.sort_unstable();
-            }
             self.items.append(&mut items);
             self.current_bin += 1;
         }
 
         (!self.items.is_empty()).then(|| std::mem::take(&mut self.items))
     }
-}
-
-/// Specify how the accounts index pubkey iterator should return pubkeys
-///
-/// Users should prefer `Unsorted`, unless required otherwise,
-/// as sorting incurs additional runtime cost.
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum AccountsIndexPubkeyIterOrder {
-    /// Returns pubkeys *not* sorted
-    Unsorted,
-    /// Returns pubkeys *sorted*
-    Sorted,
 }
 
 #[cfg(test)]
@@ -99,31 +82,22 @@ mod tests {
             );
         }
 
-        for iter_order in [
-            AccountsIndexPubkeyIterOrder::Sorted,
-            AccountsIndexPubkeyIterOrder::Unsorted,
-        ] {
-            // Create a sorted iterator for the whole pubkey range.
-            let mut iter = index.iter(iter_order);
-            // First iter.next() should return the first batch of 2000 pubkeys in the first bin.
-            let x = iter.next().unwrap();
-            assert_eq!(x.len(), 2 * ITER_BATCH_SIZE);
-            assert_eq!(
-                x.is_sorted(),
-                iter_order == AccountsIndexPubkeyIterOrder::Sorted
-            );
-            assert_eq!(iter.items.len(), 0); // should be empty.
+        // Create an iterator for the whole pubkey range.
+        let mut iter = index.iter();
+        // First iter.next() should return the first batch of 2000 pubkeys in the first bin.
+        let x = iter.next().unwrap();
+        assert_eq!(x.len(), 2 * ITER_BATCH_SIZE);
+        assert_eq!(iter.items.len(), 0); // should be empty.
 
-            // Then iter.next() should return None.
-            assert!(iter.next().is_none());
-        }
+        // Then iter.next() should return None.
+        assert!(iter.next().is_none());
     }
 
     #[test]
     fn test_accounts_iter_finished() {
         let index = AccountsIndex::<bool, bool>::default_for_tests();
         index.add_root(0);
-        let mut iter = index.iter(AccountsIndexPubkeyIterOrder::Sorted);
+        let mut iter = index.iter();
         assert!(iter.next().is_none());
         let mut gc = ReclaimsSlotList::new();
         index.upsert(
